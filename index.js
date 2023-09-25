c4d9b7faab1fe1c36ae87dd65c190c4efec05f8c
khaled.dev <salehkhaled.dev@gmail.com>
2023-09-25 13:58:07 +0300

    step4:id and search

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/time', (req, res) => {
-    const currentTime = new Date.toLocalTimeString();
+    const currentTime = new Date().toLocaleTimeString();
     res.json({status: 200, message: currentTime});
+});
+
+app.get('/hello/:id', (req, res) => {
+  const id = req.params.id;
+  if (id == "") {
+    id = "world";
+  }
+  res.json({status: 200, message: `Hello, ${ id }`});
+});
+
+app.get('/search', (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).json({status: 200, message: 'ok', data: search});
+  } else {
+    res.status(500).json({status: 500, error: true, message: 'you have to provide a search'});
+  }
 });
 
 app.listen(port, () => {
